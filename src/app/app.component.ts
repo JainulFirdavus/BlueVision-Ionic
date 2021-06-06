21cf2ba45a8c6fca8d375a62db10ca174808b486
jain252026 <mj.fridavus@innovationsapp.sa>
2021-06-06 23:20:59 +0530

    role mgmt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 
@@ -7,7 +7,75 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
-  
+export class AppComponent implements OnInit {
+  menuList: any
+  constructor() { }
+
+  ngOnInit() {
+
+    if (localStorage.getItem('token')) {
+      var uses = JSON.parse(localStorage.getItem('token'))
+      if (uses.role == 'admin') {
+        this.menuList = [
+          {
+            name: 'Dashboard',
+            icon: 'aperture-outline',
+            url: '/dashboard'
+          },
+          {
+            name: 'Orders',
+            icon: 'bicycle-outline',
+            url: '/orders'
+          },
+          {
+            name: 'Information',
+            icon: 'flame-outline',
+            url: '/customerinformation'
+          },
+          {
+            name: 'Visit',
+            icon: 'bicycle-outline',
+            url: '/customervisit'
+          },
+          {
+            name: 'Report',
+            icon: 'ribbon-outline',
+            url: '/Report'
+          },
+          {
+            name: 'Settings',
+            icon: 'settings-outline',
+            url: '/settings'
+          }, {
+            name: 'Logout',
+            icon: 'settings-outline',
+            url: 'logout'
+          },
+
+        ]
+      } else {
+        this.menuList = [
+          {
+            name: 'Orders',
+            icon: 'bicycle-outline',
+            url: '/orders'
+          },
+          {
+            name: 'Visit',
+            icon: 'bicycle-outline',
+            url: '/customervisit'
+          },
+          {
+            name: 'Logout',
+            icon: 'settings-outline',
+            url: 'logout'
+          },
+
+        ]
+      }
+    }
+
+  }
+
+
 }
